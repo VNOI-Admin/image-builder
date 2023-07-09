026f094a6c67259721f60cb1916870c1d1c2ed11
minhnhatnoe <86871862+minhnhatnoe@users.noreply.github.com>
2023-07-10 00:48:53 +0700

    Echo stuffs

--- build.sh
+++ build.sh
@@ -9,11 +9,11 @@ fi
 if [ -f local_config.sh ]; then
     echo "Using local_config.sh"
     source local_config.sh
+    echo $VARIANT, $DISTRO, $ARCH, $MIRROR, $INS_DIR, $CHROOT, $IMAGE
     exit 0
 else
     echo "local_config.sh not found, running config.sh"
     source config.sh
-    exit 0
 fi
 
 build() {
