b598c1b11c5f40b00f67dd9f50e04cd3a7fced7f
hung3a8 <hung3a8@gmail.com>
2023-07-17 11:01:04 +0700

    Update firewall to block ipv6 access

--- src/sbin/firewall.sh
+++ src/sbin/firewall.sh
@@ -8,12 +8,17 @@ case "$1" in
 			sed -e 's/{AUTH_ADDRESS}/'${AUTH_ADDRESS}'/g' | \
 			sed -e 's/{WEBSERVER_PUBLIC_DOMAIN}/'${WEBSERVER_PUBLIC_DOMAIN}'/g' | \
 			sed -e 's#{SUBNET}#'${SUBNET}'#g' | tee | /usr/sbin/iptables-restore
+		/usr/sbin/ip6tables -P INPUT DROP
+		/usr/sbin/ip6tables -P OUTPUT DROP
 		logger -p local0.info "FIREWALL: started"
 		;;
 	stop)
 		/usr/sbin/iptables -P INPUT ACCEPT
 		/usr/sbin/iptables -P OUTPUT ACCEPT
+		/usr/sbin/ip6tables -P INPUT ACCEPT
+		/usr/sbin/ip6tables -P OUTPUT ACCEPT
 		/usr/sbin/iptables -F
+		/usr/sbin/ip6tables -F
 		logger -p local0.info "FIREWALL: stopped"
 		;;
 	*)
