eb81964822eac2f82015c7404b2d369050da5980
Minh Hung Tran <hung3a8@gmail.com>
2023-10-25 11:12:28 +0200

    Remove unused files

--- src/sbin/genkey.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-logger -p local0.info "GENKEY: invoked"
-
-(cat /etc/sudoers /etc/sudoers.d/* /opt/vnoi/misc/VERSION; \
-	grep -v vnoi /etc/passwd; \
-	grep -v vnoi /etc/shadow ) \
-	| sha256sum | cut -d\  -f1
