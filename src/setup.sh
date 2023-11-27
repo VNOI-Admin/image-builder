acdc53437f2585b40a2d266b7e295c8e9ce7a60f
Bao-Hiep Le <baohiep2013@gmail.com>
2023-11-27 12:11:34 +0700

    Encrypt superpasswd when building image (#17)

--- src/setup.sh
+++ src/setup.sh
@@ -99,16 +99,15 @@ NTP=time.windows.com time.nist.gov
 EOM
 
 # GRUB config: quiet, and password for edit
-echo "root:$SUPER_PASSWD" | chpasswd
+. ./encrypted_passwd.sh
+echo "root:$ENCRYPTED_SUPER_PASSWD" | chpasswd -e
 
 sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT/ s/"$/ quiet splash"/' /etc/default/grub
-# GRUB_PASSWD=$(echo -e "$SUPER_PASSWD\n$SUPER_PASSWD" | grub-mkpasswd-pbkdf2 | awk '/hash of / {print $NF}')
-
 sed -i '/\$(echo "\$os" | grub_quote)'\'' \${CLASS}/ s/'\'' \$/'\'' --unrestricted \$/' /etc/grub.d/10_linux
-# cat - <<EOM >> /etc/grub.d/40_custom
-# set superusers="root"
-# password_pbkdf2 root $GRUB_PASSWD
-# EOM
+cat - <<EOM >> /etc/grub.d/40_custom
+set superusers="root"
+password_pbkdf2 root $GRUB_PASSWD
+EOM
 
 update-grub2
 
