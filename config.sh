18f0db5c8717d3f761343d4422507004ae5a7d29
Minh Hung Tran <hung3a8@gmail.com>
2024-01-20 16:03:44 +0100

    Update documentation

--- config.sh
+++ config.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-export ARCH="amd64" # Architecture
-export DISTRO="focal" # (DEPRECATED) Distribution, focal recommended
-export VARIANT="minbase" # Variant
-
-export MIRROR="http://vn.archive.ubuntu.com/ubuntu/" # see https://launchpad.net/ubuntu/+archivemirrors
-
 export INS_DIR="./live-build" # Installation directory
 export CHROOT=$INS_DIR/chroot
 export IMAGE=$INS_DIR/image
