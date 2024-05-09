6a65b769c1de98b9ccc9cca4805afdcf9b4302d2
Nguyen Minh Nhat Noe <86871862+minhnhatnoe@users.noreply.github.com>
2024-05-09 14:45:04 +0700

    fix: log all output of startup.sh to /opt/vnoi/startup.log

--- image-toolkit/sbin/startup.sh
+++ image-toolkit/sbin/startup.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-set -e 
+exec > /opt/vnoi/startup.log 2>&1
 
+set -e
+
+echo "Starting client"
 /opt/vnoi/bin/client &
 
-killall vlc
-killall ffmpeg
+echo "Killing vlc and ffmpeg"
+killall vlc || true
+killall ffmpeg || true
 
-cvlc -q screen:// --screen-fps=15 --sout "#transcode{vcodec=h264,acodec=none,vb=3000,ab=0}:duplicate{dst=std{access=rtmp,mux=ffmpeg{mux=flv},dst=rtmp://localhost/live/stream},dst=std{access=http,mux=ts,dst=:101}}" >/dev/null 2>&1 &
+echo "Starting cvlc"
+cvlc -q screen:// --screen-fps=15 --sout "#transcode{vcodec=h264,acodec=none,vb=3000,ab=0}:duplicate{dst=std{access=rtmp,mux=ffmpeg{mux=flv},dst=rtmp://localhost/live/stream},dst=std{access=http,mux=ts,dst=:101}}" &
 
-while ! timeout 0.1 nc -z localhost 101; do sleep 0.5; done
-ffmpeg -i http://localhost:101 -c copy -f segment -reset_timestamps 1 -strftime 1 -segment_time 120 -segment_format mp4 "/opt/vnoi/misc/records/out-%Y-%m-%d-%H-%M-%S.mp4" >/dev/null 2>&1 &
+echo "Polling for cvlc start"
+while ! timeout 0.1 nc -z localhost 101; do
+    sleep 0.5;
+done
+
+echo "Starting ffmpeg"
+ffmpeg -i http://localhost:101 -c copy -f segment -reset_timestamps 1 -strftime 1 -segment_time 120 -segment_format mp4 "/opt/vnoi/misc/records/out-%Y-%m-%d-%H-%M-%S.mp4" &
