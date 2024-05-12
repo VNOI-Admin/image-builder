b1ff2a09f08052c10229ed99241775c816f51fc5
Nguyen Minh Nhat Noe <86871862+minhnhatnoe@users.noreply.github.com>
2024-05-12 15:48:49 +0700

    fix: ffmpeg wait more for vlc

--- image-toolkit/sbin/startup.sh
+++ image-toolkit/sbin/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-exec > /opt/vnoi/startup.log 2>&1
+exec > /opt/vnoi/store/log/startup.log 2>&1
 
 echo "Starting client"
 /opt/vnoi/bin/client &
@@ -10,12 +10,20 @@ killall vlc || true
 killall ffmpeg || true
 
 echo "Starting cvlc"
-cvlc -q screen:// --screen-fps=15 --sout "#transcode{vcodec=h264,acodec=none,vb=3000,ab=0}:duplicate{dst=std{access=rtmp,mux=ffmpeg{mux=flv},dst=rtmp://localhost/live/stream},dst=std{access=http,mux=ts,dst=:101}}" &
+cvlc -q screen:// --screen-fps=15 --sout \
+    "#transcode{ \
+        vcodec=h264,acodec=none,vb=3000,ab=0 \
+    }:duplicate{ \
+        dst=std{access=rtmp,mux=ffmpeg{mux=flv},dst=rtmp://localhost/live/stream}, \
+        dst=std{access=http,mux=ts,dst=:101} \
+    }" &
 
 echo "Polling for cvlc start"
 while ! timeout 0.1 nc -z localhost 101; do
     sleep 0.5;
 done
 
+sleep 5
+
 echo "Starting ffmpeg"
-ffmpeg -i http://localhost:101 -c copy -f segment -reset_timestamps 1 -strftime 1 -segment_time 120 -segment_format mp4 "/opt/vnoi/misc/records/out-%Y-%m-%d-%H-%M-%S.mp4" &
+ffmpeg -i http://localhost:101 -re -c copy -f segment -reset_timestamps 1 -strftime 1 -segment_time 120 -segment_format mp4 "/opt/vnoi/misc/records/out-%Y-%m-%d-%H-%M-%S.mp4" &
