653c9030c062fe5ced0f173d7109c1b383591f41
Nguyen Minh Nhat Noe <86871862+minhnhatnoe@users.noreply.github.com>
2024-05-15 18:27:00 +0700

    wip: kill vlc on exit

--- image-toolkit/sbin/startup.sh
+++ image-toolkit/sbin/startup.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
-exec > /opt/vnoi/store/log/startup.log 2>&1
+exec > "/opt/vnoi/store/log/startup-$$.log" 2>&1
+
+# Check pgid and pid match
+if [ ! $$ = $(ps -o pgid -hp $$) ]; then
+    echo "Enabling job control"
+    set -m
+fi
+
+# https://stackoverflow.com/questions/360201/how-do-i-kill-background-processes-jobs-when-my-shell-script-exits/53714583#53714583
+trap "exit \$exit_code" INT TERM
+trap "exit_code=\$?; kill 0" EXIT
+
+if [ -f /run/icpc-startup.pid ]; then
+    echo "Found existing startup pid file"
+
+    STARTUP_PID=$(cat /run/icpc-startup.pid)
+    # Check if the pid is still running and whether it's me
+    if kill -0 $STARTUP_PID && diff /proc/$STARTUP_PID/cmdline /proc/$$/cmdline; then
+        echo "Killing existing startup"
+        kill -- -$(cat /run/icpc-startup.pid)
+    else
+        echo "Removing stale startup pid file"
+    fi
+
+    rm /run/icpc-startup.pid
+fi
+
+echo "Writing my own pid"
+echo $$ > /run/icpc-startup.pid
 
 echo "Starting client"
 /opt/vnoi/bin/client &
