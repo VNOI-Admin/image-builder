743697d5e03e3ba713f3458a2afb267fabdbcca1
huytrinhm <huytrinhm@gmail.com>
2024-05-18 16:03:40 +0700

    feat: add stream test

--- image-toolkit/tests.sh
+++ image-toolkit/tests.sh
@@ -41,5 +41,6 @@ run_test() {
 log "Running all tests"
 
 run_test network.test.sh
+run_test stream.test.sh
 
 log "Completed with \e[32m$PASS_COUNT PASSED\e[36m, \e[31m$(($CASE_COUNT - $PASS_COUNT)) FAILED\e[0m"
